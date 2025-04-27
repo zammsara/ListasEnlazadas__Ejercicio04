ca1bf182bfbc9eef3be596619ac1ac9ae1164e6a
AliciaEA <aliestrada@uamv.edu.ni>
2025-04-27 14:12:31 -0600

    Modulo para el menu

---
+++ mod_menu.py
@@ -0,0 +1,8 @@
+def mostrar_menu():
+    print("\n=== Menú ===")
+    print("1. Agregar texto")
+    print("2. Deshacer")
+    print("3. Rehacer")
+    print("4. Ver contenido")
+    print("5. Salir")
+    
